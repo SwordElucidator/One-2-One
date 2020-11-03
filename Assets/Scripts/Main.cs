70b027f3b790d1917432b3eb2ae75b5e500c418d
Hotaru <nagisa940216@gmail.com>
2020-11-03 16:33:25 +0800

    mopub && ironsrc

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using GoogleMobileAds.Api;
+using GoogleMobileAds.Api.Mediation.MoPub;
 
 
 public class Main : MonoBehaviour
@@ -13,6 +14,16 @@ public class Main : MonoBehaviour
     {
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(initStatus => { });
+        // Initialize the MoPub SDK.
+#if UNITY_ANDROID
+        MoPub.Initialize("6cfbf16b2ab046b58b9573ee008494d8");
+#elif UNITY_IPHONE
+        MoPub.Initialize("19743bae2b8440d2b7a891be3fb82692");
+#else
+        MoPub.Initialize("19743bae2b8440d2b7a891be3fb82692");
+#endif
+        
+        
     }
 
 }
