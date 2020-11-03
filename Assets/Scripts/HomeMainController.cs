d1e704065e4c54ccbf7bb515e352dbf7ba61e654
蒋治国 <jiangzhiguo1992@163.com>
2020-11-03 13:56:16 +0800

    update home

--- Assets/Scripts/HomeMainController.cs
+++ Assets/Scripts/HomeMainController.cs
@@ -1,18 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HomeMainController : MonoBehaviour
 {
+    // welcome
+    public GameObject welcome;
+    public Text welcomeGold;
+    // public Button btn;
+
+    // settings
+    public GameObject settings;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void Init()
+    {
+        CheckWelcome();
+        // TODO money(unit) + gold
+        // TODO score + champion
+        // TODO actionValue
+        // TODO 来点动画？
+    }
+
+    private void CheckWelcome()
+    {
+        const bool show = true; // TODO perGet
+        welcome.SetActive(show);
+        welcomeGold.text = "100"; // TODO value
+    }
+
+    public void HideWelcome()
+    {
+        welcome.SetActive(false);
+        // btn.onClick.AddListener();
+        // TODO perSet
+    }
+
+    public void SettingsPress()
+    {
+        settings.SetActive(true);
+        // TODO nameValue
+    }
+
+    public void MoneyInfoPress()
+    {
+        // TODO nav
+    }
+
+    public void GoldInfoPress()
+    {
+        // 应该是没有跳转
+    }
+
+    public void GameSinglePress()
+    {
+        // TODO nav
+    }
+
+    public void MultiSinglePress()
+    {
+        // TODO nav
     }
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,87 @@
+using System;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        Debug.Log("第一个场景加载前");
+    }
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void OnAfterSceneLoadRuntimeMethod()
+    {
+        Debug.Log("第一个场景加载后");
+    }
+
     [RuntimeInitializeOnLoadMethod]
     static void OnRuntimeMethodLoad()
     {
+        Debug.Log("第一个场景加载后");
     }
 
+    private void Reset()
+    {
+        Debug.Log("Reset");
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    private void FixedUpdate()
+    {
+        // Debug.Log("FixedUpdate");
+    }
+
+    private void Update()
+    {
+        // Debug.Log("Update");
+    }
+
+    private void LateUpdate()
+    {
+        // Debug.Log("LateUpdate");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        Debug.Log("OnApplicationFocus");
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        Debug.Log("OnApplicationPause");
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+    }
+
+    private void OnGUI()
+    {
+    }
 }
