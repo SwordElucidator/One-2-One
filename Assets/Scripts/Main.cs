e1825d870a970be7dd56f960f2ead1c47f32767f
蒋治国 <jiangzhiguo1992@163.com>
2020-11-03 18:07:32 +0800

    fix main

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,8 +1,6 @@
-using System;
-using UnityEngine;
 using GoogleMobileAds.Api;
 using GoogleMobileAds.Api.Mediation.MoPub;
-
+using UnityEngine;
 
 public class Main : MonoBehaviour
 {
@@ -42,6 +40,17 @@ public class Main : MonoBehaviour
     private void Start()
     {
         Debug.Log("Start");
+        
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+        // Initialize the MoPub SDK.
+#if UNITY_ANDROID
+        MoPub.Initialize("6cfbf16b2ab046b58b9573ee008494d8");
+#elif UNITY_IPHONE
+        MoPub.Initialize("19743bae2b8440d2b7a891be3fb82692");
+#else
+        MoPub.Initialize("19743bae2b8440d2b7a891be3fb82692");
+#endif
     }
 
     private void FixedUpdate()
@@ -69,22 +78,6 @@ public class Main : MonoBehaviour
         Debug.Log("OnDisable");
     }
     
-    public void Start()
-    {
-        // Initialize the Google Mobile Ads SDK.
-        MobileAds.Initialize(initStatus => { });
-        // Initialize the MoPub SDK.
-#if UNITY_ANDROID
-        MoPub.Initialize("6cfbf16b2ab046b58b9573ee008494d8");
-#elif UNITY_IPHONE
-        MoPub.Initialize("19743bae2b8440d2b7a891be3fb82692");
-#else
-        MoPub.Initialize("19743bae2b8440d2b7a891be3fb82692");
-#endif
-        
-        
-    }
-
     private void OnApplicationFocus(bool hasFocus)
     {
         Debug.Log("OnApplicationFocus");
