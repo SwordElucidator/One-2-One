402975ae1b4aeef9a92093850e70e95af3c52145
蒋治国 <jiangzhiguo1992@163.com>
2020-11-03 14:21:21 +0800

    update home_script

--- Assets/Scripts/HomeMainController.cs
+++ Assets/Scripts/HomeMainController.cs
@@ -3,71 +3,133 @@ using UnityEngine.UI;
 
 public class HomeMainController : MonoBehaviour
 {
+    // main
+
     // welcome
     public GameObject welcome;
+
     public Text welcomeGold;
     // public Button btn;
 
     // settings
     public GameObject settings;
 
-    // Start is called before the first frame update
+    // agreement
+    public GameObject agreement;
+
     void Start()
     {
         Init();
     }
 
-    // Update is called once per frame
     void Update()
     {
     }
 
     private void Init()
     {
-        CheckWelcome();
+        CheckWelcomeVisible();
         // TODO money(unit) + gold
         // TODO score + champion
         // TODO actionValue
         // TODO 来点动画？
     }
 
-    private void CheckWelcome()
+    /**
+     ***************************************** Welcome ******************************************* 
+     */
+    private void CheckWelcomeVisible()
     {
         const bool show = true; // TODO perGet
         welcome.SetActive(show);
         welcomeGold.text = "100"; // TODO value
     }
 
-    public void HideWelcome()
+    public void OnWelcomeHidePress()
     {
         welcome.SetActive(false);
         // btn.onClick.AddListener();
         // TODO perSet
     }
 
-    public void SettingsPress()
+    /**
+     ***************************************** Settings ******************************************* 
+     */
+    public void OnSettingsShowPress()
     {
         settings.SetActive(true);
         // TODO nameValue
     }
 
-    public void MoneyInfoPress()
+    public void OnSettingsHidePress()
+    {
+        settings.SetActive(false);
+    }
+
+    public void OnSettingsAvatarPress()
+    {
+        // TODO selectAvatar
+    }
+
+    public void OnSettingsNameSavePress()
+    {
+        // TODO saveName
+    }
+
+    /**
+     ***************************************** Agreement ******************************************* 
+     */
+    public void OnAgreementShowPress()
+    {
+        agreement.SetActive(true);
+    }
+
+    public void OnAgreementHidePress()
+    {
+        agreement.SetActive(true);
+    }
+
+    /**
+     ***************************************** Bill ******************************************* 
+     */
+    public void OnBillShowPress()
     {
         // TODO nav
     }
 
-    public void GoldInfoPress()
-    {
-        // 应该是没有跳转
-    }
-
-    public void GameSinglePress()
+    public void OnBillHidePress()
     {
         // TODO nav
     }
 
-    public void MultiSinglePress()
+    public void OnBillCashOutPress()
     {
         // TODO nav
+    }
+
+    public void BillCashOutOver()
+    {
+        // TODO refreshView
+    }
+
+    public void OnBillSpeedUpPress()
+    {
+        // TODO ad + finishAlert
+    }
+
+    /**
+     ***************************************** Single ******************************************* 
+     */
+    public void OnGameSinglePress()
+    {
+        // TODO nav
+    }
+
+    /**
+     ***************************************** Multi ******************************************* 
+     */
+    public void OnMultiSinglePress()
+    {
+        // TODO check + alert + nav
     }
 }
