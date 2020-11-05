3548460100417152629f044a69043b21f218eab7
蒋治国 <jiangzhiguo1992@163.com>
2020-11-05 19:09:47 +0800

    update home_play

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -3,6 +3,9 @@ public static class Config
     public static readonly int DaySignCoin = 60; // 每日登录
     public static readonly int MultiNeedCoin = 20;
     public static readonly int MultiNeedMoney = 2;
+    public static readonly int MultiNeedPeoples = 20;
+    public static readonly int MultiNo1Award = 12;
+    public static readonly int MultiNo27Award = 1;
 
     public static string GetUnit()
     {
